96c38da3b4ada2069e4df8ae8fa9dc39494ff55e
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-06-30 01:10:28 +0200

    Rewrite advisor to a more simpler form

--- advisor/advisor.js
+++ advisor/advisor.js
@@ -1,40 +1,25 @@
-module.exports = (function() {
+var advisor = module.exports;
 
-    var mod = {};
-    var statemap;
+/**
+ * Returns the best keepers to choose from the given game state.
+ * @param scorecard The scorecard represented as a 15-integer array.
+ * @param upperScore The upper score.
+ * @param dice The 5 dice currently held by the player.
+ * @param rollsLeft The number of rolls left before scoring.
+ * @returns {Array.<number>} The best keepers to choose from the given game state.
+ */
+advisor.getBestKeepers = function(scorecard, upperScore, dice, rollsLeft) {
+    rand = Math.floor((Math.random() * 5) + 1);
+    return [0, 1, 2, 3, 4].slice(0, rand);
+}
 
-    /**
-     * Initializes the module.
-     * @param conf An object containing the settings.
-     */
-    mod.init = function(conf) {
-        if (!!conf.statemap) statemap = conf.statemap;
-    };
-
-    /**
-     * Returns the best keepers to choose from the given game state.
-     * @param scorecard The scorecard represented as a 15-integer array.
-     * @param upperScore The upper score.
-     * @param dice The 5 dice currently held by the player.
-     * @param rollsLeft The number of rolls left before scoring.
-     * @returns {Array.<number>} The best keepers to choose from the given game state.
-     */
-    mod.getBestKeepers = function(scorecard, upperScore, dice, rollsLeft) {
-        rand = Math.floor((Math.random() * 5) + 1);
-        return [0, 1, 2, 3, 4].slice(0, rand);
-    };
-
-    /**
-     * Returns the best category to score in from the given game state.
-     * @param scorecard The scorecard represented as a 15-integer array.
-     * @param upperScore The upper score.
-     * @param dice The 5 dice currently held by the player.
-     * @returns {Number|number} The best category to score in from the given game state.
-     */
-    mod.getBestCategory = function(scorecard, upperScore, dice) {
-        return scorecard.indexOf(0);
-    };
-
-    return mod;
-
-})();
+/**
+ * Returns the best category to score in from the given game state.
+ * @param scorecard The scorecard represented as a 15-integer array.
+ * @param upperScore The upper score.
+ * @param dice The 5 dice currently held by the player.
+ * @returns {Number|number} The best category to score in from the given game state.
+ */
+advisor.getBestCategory = function(scorecard, upperScore, dice) {
+    return scorecard.indexOf(0);
+}
