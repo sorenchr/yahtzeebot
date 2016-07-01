375a40caae8d3a4b56a42ecc8860bd20c33a94cc
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-07-01 15:58:39 +0200

    Add StateMap implementation for holding EV's based on scorecard and upperscore pairs

---
+++ advisor/statemap.js
@@ -0,0 +1,36 @@
+var StateMap = function() {
+    this.map = {};
+}
+
+function getKey(scorecard, upperScore) {
+    return scorecard.join('') + '|' + upperScore;
+}
+
+StateMap.prototype.getEV = function(scorecard, upperScore) {
+    var key = getKey(scorecard, upperScore);
+    return this.map[key];
+}
+
+StateMap.prototype.addEV = function (scorecard, upperScore, ev) {
+    var key = getKey(scorecard, upperScore);
+    this.map[key] = ev;
+}
+
+StateMap.prototype.toJSON = function() {
+    return this.map;
+}
+
+StateMap.fromJSON = function(json) {
+    var map = new StateMap();
+
+    for (var key in json) {
+        var keySplit = key.split('|');
+        var scorecard = keySplit[0].split('');
+        var upperScore = keySplit[1];
+        map.addEV(scorecard, upperScore, json[key]);
+    }
+
+    return map;
+}
+
+module.exports = StateMap;
