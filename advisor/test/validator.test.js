d1cc423eec3b40c3871c346b49ec511db694b23c
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-07-12 13:52:01 +0200

    Flesh out validator tests

--- advisor/test/validator.test.js
+++ advisor/test/validator.test.js
@@ -9,7 +9,8 @@ describe('validator', function() {
         });
 
         it('should return false on array with non-integer elements', function() {
-            assert.isFalse(validator.isValidDice([2,1,'a']));
+            assert.isFalse(validator.isValidDice([2,1,'abc']));
+            assert.isFalse(validator.isValidDice([2,1,'2']));
             assert.isFalse(validator.isValidDice([2,1,{}]));
             assert.isFalse(validator.isValidDice([2,1,null]));
             assert.isFalse(validator.isValidDice([2,1,true]));
@@ -25,6 +26,7 @@ describe('validator', function() {
         });
 
         it('should return true on valid array', function() {
+            assert.isTrue(validator.isValidDice([1,2,3,4,5,6]));
             assert.isTrue(validator.isValidDice([1,2,3,4,5]));
             assert.isTrue(validator.isValidDice([5,5,5,5,5]));
             assert.isTrue(validator.isValidDice([1,2,3,4]));
