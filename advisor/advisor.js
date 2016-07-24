2634b0c8794572faa4c95ad237814d7831bf7fc1
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-07-25 00:59:12 +0200

    Add test cases covering getBestKeepers() for the advisor module and an implementation that satisfies the testcases so far

--- advisor/advisor.js
+++ advisor/advisor.js
@@ -1,7 +1,20 @@
 var validator = require('./validator');
 var ArgumentError = require('./argumenterror');
+var _ = require('lodash');
+var scorecalc = require('./score-calculator');
 
 var advisor = module.exports;
+
+// The StateMap used for EV lookups
+var stateMap;
+
+/**
+ * Initialize the module.
+ * @param config An object containing settings.
+ */
+advisor.init = function(config) {
+    stateMap = config['stateMap'];
+};
 
 /**
  * Returns the best keepers to choose from the given game state.
@@ -31,4 +44,47 @@ advisor.getBestCategory = function(scorecard, upperScore, dice) {
     if (!validator.isValidScorecard(scorecard)) throw new ArgumentError('Invalid scorecard: ' + scorecard);
     if (!validator.isValidUpperScore(upperScore)) throw new ArgumentError('Invalid upper score: ' + upperScore);
     if (!validator.isValidDice(dice)) throw new ArgumentError('Invalid dice: ' + dice);
+
+    // Initialize variables to keep track of the best possible category
+    var bestCategory = 0, bestCategoryEV = 0;
+
+    // Iterate through each unmarked category
+    for (var i = 0; i < scorecard.length; i++) {
+        // Skip marked categories
+        if (scorecard[i] != 0) continue;
+
+        // Create a new scorecard where the category is marked
+        var newScorecard = getMarkedScorecard(scorecard, i);
+
+        // Find the new upper score from scoring in this category
+        var categoryScore = scorecalc.getCategoryScore(i, dice);
+        var isUpperCategory = i >= 0 && i <= 5;
+        var newUpperScore = isUpperCategory ? upperScore + categoryScore : upperScore;
+
+        // Calculate the category EV
+        var categoryEV = stateMap.getEV(newScorecard, newUpperScore) + categoryScore;
+
+        // Check if scoring the category results in the upper section bonus
+        if (upperScore < 63 && newUpperScore >= 63) categoryEV += 50;
+
+        // Check if this is the best category so far
+        if (categoryEV >= bestCategoryEV) {
+            bestCategory = i;
+            bestCategoryEV = categoryEV;
+        }
+    }
+
+    return bestCategory;
 };
+
+/**
+ * Clones the given scorecard and returns the clone with the marked category.
+ * @param scorecard The scorecard to clone.
+ * @param i The category to mark as scored.
+ * @returns {Array} The cloned scorecard with the marked category.
+ */
+function getMarkedScorecard(scorecard, i) {
+    var newScorecard = _.clone(scorecard);
+    newScorecard[i] = true;
+    return newScorecard;
+}
--- advisor/test/advisor.test.js
+++ advisor/test/advisor.test.js
@@ -4,77 +4,236 @@ var proxyquire = require('proxyquire').noPreserveCache();
 var ArgumentError = require('../argumenterror');
 
 describe('advisor', function() {
+    describe('#init', function() {
+        it('should use the StateMap provided in the settings', function() {
+
+        });
+    });
+
     describe('#getBestKeepers', function () {
         it('should throw ArgumentError on invalid scorecard', function() {
             var validatorMock = { isValidScorecard: function(scorecard) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestKeepers.bind(advisor, scorecard, 55, dice, 1), ArgumentError);
+            assert.throws(advisorProxy.getBestKeepers.bind(advisorProxy, scorecard, 55, dice, 1), ArgumentError);
         });
 
         it('should throw ArgumentError on invalid upper score', function() {
             var validatorMock = { isValidUpperScore: function(upperScore) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestKeepers.bind(advisor, scorecard, 55, dice, 1), ArgumentError);
+            assert.throws(advisorProxy.getBestKeepers.bind(advisorProxy, scorecard, 55, dice, 1), ArgumentError);
         });
 
         it('should throw ArgumentError on invalid dice', function() {
             var validatorMock = { isValidDice: function(dice) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestKeepers.bind(advisor, scorecard, 55, dice, 1), ArgumentError);
+            assert.throws(advisorProxy.getBestKeepers.bind(advisorProxy, scorecard, 55, dice, 1), ArgumentError);
         });
 
         it('should throw ArgumentError on invalid rolls left', function() {
             var validatorMock = { isValidRollsLeft: function(rollsLeft) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestKeepers.bind(advisor, scorecard, 55, dice, 1), ArgumentError);
+            assert.throws(advisorProxy.getBestKeepers.bind(advisorProxy, scorecard, 55, dice, 1), ArgumentError);
         });
     });
 
     describe('#getBestCategory', function() {
+        var advisor;
+
         it('should throw ArgumentError on invalid scorecard', function() {
             var validatorMock = { isValidScorecard: function(scorecard) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestCategory.bind(advisor, scorecard, 55, dice), ArgumentError);
+            assert.throws(advisorProxy.getBestCategory.bind(advisorProxy, scorecard, 55, dice), ArgumentError);
         });
 
         it('should throw ArgumentError on invalid upper score', function() {
             var validatorMock = { isValidUpperScore: function(upperScore) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestCategory.bind(advisor, scorecard, 55, dice), ArgumentError);
+            assert.throws(advisorProxy.getBestCategory.bind(advisorProxy, scorecard, 55, dice), ArgumentError);
         });
 
         it('should throw ArgumentError on invalid dice', function() {
             var validatorMock = { isValidDice: function(dice) { return false } };
-            var advisor = proxyquire('../advisor', { './validator': validatorMock });
+            var advisorProxy = proxyquire('../advisor', { './validator': validatorMock });
 
             var scorecard = new Array(15).fill(true);
             var dice = [1,2,3,4,5];
 
-            assert.throws(advisor.getBestCategory.bind(advisor, scorecard, 55, dice), ArgumentError);
+            assert.throws(advisorProxy.getBestCategory.bind(advisorProxy, scorecard, 55, dice), ArgumentError);
+        });
+
+        it('should choose the category with the best EV', function() {
+            var scorecardToMatch = '000000000000001';
+
+            var stateMapMock = {
+                getEV: function(scorecard, upperScore) {
+                    var scorecardString = scorecard.map(x => x ? 1 : 0).join('');
+                    if (scorecardString === scorecardToMatch) return 2.0;
+                    return 1.0;
+                }
+            };
+
+            var scoreCalcMock = {
+                getCategoryScore: function(category, dice) { return 0; }
+            };
+
+            var advisorProxy = proxyquire('../advisor', { './score-calculator': scoreCalcMock });
+            advisorProxy.init({ 'stateMap': stateMapMock });
+
+            var scorecard = new Array(15).fill(false);
+            var upperScore = 25;
+            var dice = [1,2,3,4,5];
+
+            // The advisor should suggest scoring the last category
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 14);
+
+            // The advisor should suggest scoring the 12th category
+            scorecardToMatch = '000000000001000';
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 11);
+        });
+
+        it('should choose the category with the best score', function() {
+            var stateMapMock = {
+                getEV: function(scorecard, upperScore) { return 0; }
+            };
+
+            var bestCategory = 5;
+            var scoreCalcMock = {
+                getCategoryScore: function(category, dice) {
+                    if (category === bestCategory) return 1;
+                    return 0;
+                }
+            };
+
+            var advisorProxy = proxyquire('../advisor', { './score-calculator': scoreCalcMock });
+            advisorProxy.init({ 'stateMap': stateMapMock });
+
+            var scorecard = new Array(15).fill(false);
+            var upperScore = 25;
+            var dice = [1,2,3,4,5];
+
+            // The advisor should suggest scoring the best category
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), bestCategory);
+
+            // The advisor should still suggest scoring the best category
+            bestCategory = 12;
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), bestCategory);
+        });
+
+        it('should choose the category with the best EV and score', function() {
+            var stateMapMock = {
+                getEV: function(scorecard, upperScore) {
+                    var scorecardString = scorecard.map(x => x ? 1 : 0).join('');
+                    if (scorecardString === '010000000000000') return 40.0;
+                    if (scorecardString === '001000000000000') return 20.0;
+                    return 0.0;
+                }
+            };
+
+            var scoreCalcMock = {
+                getCategoryScore: function(category, dice) {
+                    if (category === 7) return 35.0;
+                    if (category === 2) return 25.0;
+                    return 0;
+                }
+            };
+
+            var advisorProxy = proxyquire('../advisor', { './score-calculator': scoreCalcMock });
+            advisorProxy.init({ 'stateMap': stateMapMock });
+
+            var scorecard = new Array(15).fill(false);
+            var upperScore = 25;
+            var dice = [1,2,3,4,5];
+
+            // The advisor should suggest scoring the 3rd category
+            // because the sum of the EV and score is greater than scoring one of the
+            // two by itself
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 2);
+        });
+
+        it('should skip marked categories', function() {
+            var stateMapMock = {
+                getEV: function(scorecard, upperScore) {
+                    var scorecardString = scorecard.map(x => x ? 1 : 0).join('');
+                    if (scorecardString === '000000000000001') return 5.0;
+                    if (scorecardString === '000000010000001') return 2.0;
+                    return 1.0;
+                }
+            };
+
+            var scoreCalcMock = {
+                getCategoryScore: function(category, dice) { return 0; }
+            };
+
+            var advisorProxy = proxyquire('../advisor', { './score-calculator': scoreCalcMock });
+            advisorProxy.init({ 'stateMap': stateMapMock });
+
+            var scorecard = new Array(15).fill(false);
+            scorecard[14] = true;
+            var upperScore = 55;
+            var dice = [1,2,3,4,5];
+
+            // The advisor should suggest scoring the 8th category because
+            // it should skip the 15th, even though it has greater EV
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 7);
+        });
+
+        it('should account for the upper section bonus of 50', function() {
+            var returnEV = 60.9;
+
+            var stateMapMock = {
+                getEV: function(scorecard, upperScore) {
+                    var scorecardString = scorecard.map(x => x ? 1 : 0).join('');
+                    if (scorecardString === '111110000000001') return returnEV;
+                    return 1.0;
+                }
+            };
+
+            var scoreCalcMock = {
+                getCategoryScore: function(category, dice) {
+                    if (category === 5) return 10;
+                    return 0;
+                }
+            };
+
+            var advisorProxy = proxyquire('../advisor', { './score-calculator': scoreCalcMock });
+            advisorProxy.init({ 'stateMap': stateMapMock });
+
+            var scorecard = '111110000000000'.split('').map(x => x === '1');
+            var upperScore = 55;
+            var dice = [6,6,6,6,6];
+
+            // The advisor should suggest scoring the 6th category because
+            // it triggers the upper section bonus of 50
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 5);
+
+            // The advisor should now suggest scoring in the 15th category
+            // since the EV just tops the upper section bonus slightly
+            returnEV = 61.1;
+            assert.equal(advisorProxy.getBestCategory(scorecard, upperScore, dice), 14);
         });
     });
 });
