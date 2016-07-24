d902ec13cdb87a9b94ee1f2a05a3bbd2cbe8b25b
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-07-24 15:10:06 +0200

    Simplify score-calculator tests

--- advisor/test/score-calculator.test.js
+++ advisor/test/score-calculator.test.js
@@ -2,18 +2,17 @@ var chai = require('chai');
 var assert = chai.assert;
 var proxyquire = require('proxyquire').noPreserveCache();
 var ArgumentError = require('../argumenterror');
+var scorecalc = require('../score-calculator');
 
 describe('score-calculator', function() {
     describe('#getCategoryScore', function() {
         describe('ones', function() {
             it('should return 3', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 1, 1, 3, 1];
                 assert.equal(scorecalc.getCategoryScore(0, dice), 3);
             });
 
             it('should return 5', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 1, 1, 1];
                 assert.equal(scorecalc.getCategoryScore(0, dice), 5);
             });
@@ -21,13 +20,11 @@ describe('score-calculator', function() {
 
         describe('twos', function() {
             it('should return 4', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 1, 3, 1];
                 assert.equal(scorecalc.getCategoryScore(1, dice), 4);
             });
 
             it('should return 8', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 1, 2, 2];
                 assert.equal(scorecalc.getCategoryScore(1, dice), 8);
             });
@@ -35,13 +32,11 @@ describe('score-calculator', function() {
 
         describe('threes', function() {
             it('should return 3', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 1, 3, 1];
                 assert.equal(scorecalc.getCategoryScore(2, dice), 3);
             });
 
             it('should return 9', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [3, 2, 3, 2, 3];
                 assert.equal(scorecalc.getCategoryScore(2, dice), 9);
             });
@@ -49,13 +44,11 @@ describe('score-calculator', function() {
 
         describe('fours', function() {
             it('should return 8', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 4, 3, 4];
                 assert.equal(scorecalc.getCategoryScore(3, dice), 8);
             });
 
             it('should return 20', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [4, 4, 4, 4, 4];
                 assert.equal(scorecalc.getCategoryScore(3, dice), 20);
             });
@@ -63,13 +56,11 @@ describe('score-calculator', function() {
 
         describe('fours', function() {
             it('should return 8', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 4, 3, 4];
                 assert.equal(scorecalc.getCategoryScore(3, dice), 8);
             });
 
             it('should return 20', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [4, 4, 4, 4, 4];
                 assert.equal(scorecalc.getCategoryScore(3, dice), 20);
             });
@@ -77,13 +68,11 @@ describe('score-calculator', function() {
 
         describe('fives', function() {
             it('should return 15', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [5, 5, 4, 5, 4];
                 assert.equal(scorecalc.getCategoryScore(4, dice), 15);
             });
 
             it('should return 25', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [5, 5, 5, 5, 5];
                 assert.equal(scorecalc.getCategoryScore(4, dice), 25);
             });
@@ -91,13 +80,11 @@ describe('score-calculator', function() {
 
         describe('sixes', function() {
             it('should return 12', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 6, 4, 5, 4];
                 assert.equal(scorecalc.getCategoryScore(5, dice), 12);
             });
 
             it('should return 24', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 6, 6, 6, 5];
                 assert.equal(scorecalc.getCategoryScore(5, dice), 24);
             });
@@ -105,13 +92,11 @@ describe('score-calculator', function() {
 
         describe('one pair', function() {
             it('should return 6', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 6, 3, 3];
                 assert.equal(scorecalc.getCategoryScore(6, dice), 6);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 2, 3, 4, 5];
                 assert.equal(scorecalc.getCategoryScore(6, dice), 0);
             });
@@ -119,13 +104,11 @@ describe('score-calculator', function() {
 
         describe('two pairs', function() {
             it('should return 14', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 6, 6, 3];
                 assert.equal(scorecalc.getCategoryScore(7, dice), 14);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 6, 5, 3];
                 assert.equal(scorecalc.getCategoryScore(7, dice), 0);
             });
@@ -133,13 +116,11 @@ describe('score-calculator', function() {
 
         describe('three of a kind', function() {
             it('should return 9', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [3, 3, 3, 2, 6];
                 assert.equal(scorecalc.getCategoryScore(8, dice), 9);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [3, 3, 2, 2, 6];
                 assert.equal(scorecalc.getCategoryScore(8, dice), 0);
             });
@@ -147,13 +128,11 @@ describe('score-calculator', function() {
 
         describe('four of a kind', function() {
             it('should return 8', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [3, 2, 2, 2, 2];
                 assert.equal(scorecalc.getCategoryScore(9, dice), 8);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [3, 2, 2, 1, 2];
                 assert.equal(scorecalc.getCategoryScore(9, dice), 0);
             });
@@ -161,13 +140,11 @@ describe('score-calculator', function() {
 
         describe('small straight', function() {
             it('should return 15', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 2, 3, 4, 5];
                 assert.equal(scorecalc.getCategoryScore(10, dice), 15);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 2, 3, 3, 5];
                 assert.equal(scorecalc.getCategoryScore(10, dice), 0);
             });
@@ -175,13 +152,11 @@ describe('score-calculator', function() {
 
         describe('large straight', function() {
             it('should return 20', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 3, 4, 5, 6];
                 assert.equal(scorecalc.getCategoryScore(11, dice), 20);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 3, 4, 5, 6];
                 assert.equal(scorecalc.getCategoryScore(11, dice), 0);
             });
@@ -189,19 +164,16 @@ describe('score-calculator', function() {
 
         describe('full house', function() {
             it('should return 13', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 3, 3, 3];
                 assert.equal(scorecalc.getCategoryScore(12, dice), 13);
             });
 
             it('should return 27', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 6, 5, 5, 5];
                 assert.equal(scorecalc.getCategoryScore(12, dice), 27);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 2, 3, 3, 3];
                 assert.equal(scorecalc.getCategoryScore(12, dice), 0);
             });
@@ -209,13 +181,11 @@ describe('score-calculator', function() {
 
         describe('chance', function() {
             it('should return 27', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [6, 6, 5, 5, 5];
                 assert.equal(scorecalc.getCategoryScore(13, dice), 27);
             });
 
             it('should return 7', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [2, 2, 1, 1, 1];
                 assert.equal(scorecalc.getCategoryScore(13, dice), 7);
             });
@@ -223,19 +193,16 @@ describe('score-calculator', function() {
 
         describe('yahtzee', function() {
             it('should return 55', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 1, 1, 1];
                 assert.equal(scorecalc.getCategoryScore(14, dice), 55);
             });
 
             it('should return 75', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [5, 5, 5, 5, 5];
                 assert.equal(scorecalc.getCategoryScore(14, dice), 75);
             });
 
             it('should return 0', function() {
-                var scorecalc = require('../score-calculator');
                 var dice = [1, 1, 4, 1, 1];
                 assert.equal(scorecalc.getCategoryScore(14, dice), 0);
             });
@@ -246,8 +213,8 @@ describe('score-calculator', function() {
                 isValidRoll: function(roll) { return true },
                 isValidCategory: function(category) { return false }
             };
-            var scorecalc = proxyquire('../score-calculator', { './validator': validatorMock });
-            assert.throws(scorecalc.getCategoryScore.bind(scorecalc, 0, [1,2,3,4,5]), ArgumentError);
+            var proxyScorecalc = proxyquire('../score-calculator', { './validator': validatorMock });
+            assert.throws(proxyScorecalc.getCategoryScore.bind(proxyScorecalc, 0, [1,2,3,4,5]), ArgumentError);
         });
 
         it('should throw error on invalid roll', function() {
@@ -255,8 +222,8 @@ describe('score-calculator', function() {
                 isValidRoll: function(roll) { return false },
                 isValidCategory: function(category) { return true }
             };
-            var scorecalc = proxyquire('../score-calculator', { './validator': validatorMock });
-            assert.throws(scorecalc.getCategoryScore.bind(scorecalc, 0, [1,2,3,4,5]), ArgumentError);
+            var proxyScorecalc = proxyquire('../score-calculator', { './validator': validatorMock });
+            assert.throws(proxyScorecalc.getCategoryScore.bind(proxyScorecalc, 0, [1,2,3,4,5]), ArgumentError);
         });
     });
 });
