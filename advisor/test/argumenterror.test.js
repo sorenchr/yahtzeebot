4fc8328cadd0b940c34a92a28de1908d32fea28b
Søren Qvist Christensen <soren.qvist@gmail.com>
2016-07-27 11:32:07 +0200

    Clean up ArgumentError tests

--- advisor/test/argumenterror.test.js
+++ advisor/test/argumenterror.test.js
@@ -10,14 +10,4 @@ describe('ArgumentError', function() {
             assert.equal(err.message, msg);
         });
     });
-
-    describe('#toString', function() {
-        it('should output a correctly formatted string', function() {
-            var ArgumentError = require('../argumenterror');
-            var msg = 'This is a test message';
-            var err = new ArgumentError(msg);
-            var expected = 'ArgumentError: ' + msg;
-            assert.equal(err.toString(), expected);
-        });
-    });
 });
